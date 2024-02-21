b92f6f2ed054fc7b31d2c50c0fd5f451c7b9be06
ilya <cankillah@gmail.com>
2024-02-21 17:38:27 +0200

    reverted viewport onload play

--- src/Entry.ts
+++ src/Entry.ts
@@ -47,6 +47,7 @@ class Entry {
               document.getElementById("loader").style.display = 'none';
 
               const viewport = document.querySelector("video");
+              viewport.onloadedmetadata = viewport.play;        
               viewport.srcObject = stream;
               viewport.style.display = 'flex';
 

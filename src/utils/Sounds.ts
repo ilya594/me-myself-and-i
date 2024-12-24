43089635a5d984ab615915d47ee03c47c9d7a878
ilya <cankillah@gmail.com>
2024-12-24 15:56:54 +0200

    add sounds list

--- src/utils/Sounds.ts
+++ src/utils/Sounds.ts
@@ -24,6 +24,8 @@ class Sounds extends Events.EventHandler {
         0.5, 0.5, 0.5, 0.5 
     ];
 
+    private list: Array<any> = [];
+
     constructor() {
         super();              
         this._timeouts.set(Events.MOTION_DETECTED, { instance: null, delay: 7777 });
@@ -34,7 +36,29 @@ class Sounds extends Events.EventHandler {
         //@ts-ignore
         const context: AudioContext | any = new (window.AudioContext || window.webkitAudioContext)();
 
+        const getAudioList = (url: string) => {
+            return new Promise((resolve, reject) => {
+                const request = new XMLHttpRequest();
+                request.open("GET", url, true);
+              //  request.responseType = "arraybuffer";
+                request.onload = () => resolve(request.response);
+                request.onerror = (e) => reject(e);
+                request.send();
+              }); 
+        }
+
+        let audios;
+
+        try {
+            audios =  JSON.parse(await getAudioList('./audios.json') as string).list;
+        } catch (e: any) {
+            console.log('[Viewer] Sounds.initialize. error while handling config.');
+        }
+
+
+
         console.log('[Viewer] Sounds.initialize. context here.')
+
         const loadAudio = (url: string) => {
             return new Promise((resolve, reject) => {
               const request = new XMLHttpRequest();
@@ -46,25 +70,29 @@ class Sounds extends Events.EventHandler {
             });
           }
 
-        const buildAudio = async () => {
+        const buildAudio = async (blob: any) => {
             source.buffer = await context.decodeAudioData(blob.slice(0));
             source.connect(context.destination);
             return source;
         }
 
+        for (let audio in audios) {
+            this.list.push(await loadAudio(audio + ".mp3"));
+        }
+
         let source: any = null;
         let audio: any = null;  
-        let blob: any = await loadAudio("./images/les-podervanskij-kazka-pro-repku_(mufm.me).mp3");
+      //  let blob: any = await loadAudio("./images/les-podervanskij-kazka-pro-repku_(mufm.me).mp3");
       
         MotionDetector.addEventListener(Events.MOTION_DETECTION_STARTED, async () => {
             if (this._timeout) return console.log('[Sounds] Motion detect handler. Sound not played cuz of timeout');
             this._timeout = setTimeout(() => this._timeout = clearTimeout(this._timeout), SOUND_PLAY_TIME * MAYBE);
             source = context.createBufferSource();
-            audio = await buildAudio();
+            audio = await buildAudio(Math.floor(Math.random() * this.list.length));
             if (!audio?.buffer) return console.log('[Sounds] Motion detect handler. Sound not played because of no audio buffer');
             const duration: number = SOUND_PLAY_TIME * MAYBE;
-            const start: number = Math.random() * (audio.buffer.duration - Number(duration));  
-         //   audio.start(0, Math.floor(start), Math.floor(duration));
+            //const start: number = Math.random() * (audio.buffer.duration - Number(duration));  
+            audio.start(0, 0, audio.duration);
         });
     }
 
